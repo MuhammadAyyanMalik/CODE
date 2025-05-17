13d73f27dbfd6ae70dac75a7bbec5adc4879cf9a
MuhammadAyyanMalik <muhammadayyan642010@gmail.com>
2025-05-17 11:12:33 +0500

    Create variables_practice.py

---
+++ variables_practice.py
@@ -0,0 +1,13 @@
+
+name = "Ayyan"
+age = 14
+course_name = "Pyton for begineers"
+student_name = "Ayyan"
+student_age = 14
+total_marks = 95
+print("Name:", name)
+print("Age:", age)
+print("Course:", course_name)
+print("Student Name:", student_name)
+print("Student Age:", student_age)
+print("Total Marks:", total_marks)
